fabd58e3b085e61913c653aff9debe4ca9c5d81d
Davide Moro <davide.moro@gmail.com>
2014-01-29 00:14:57 +0100

    fixed tests (todo: more tests)

--- test/spec/directives/smartcheckbox.js
+++ test/spec/directives/smartcheckbox.js
@@ -6,55 +6,60 @@ describe('Directive: smartCheckbox', function () {
   beforeEach(module('angularjsSmartcheckboxApp', 'views/smartcheckbox.html'));
 
   var element,
-    $scope;
+    $compile,
+    scope;
 
-  beforeEach(inject(function ($rootScope, $compile) {
-    $scope = $rootScope.$new();
-    $scope.model = [
-      {id: '001', label:'First item'},
-      {id: '002', label:'Second item'},
-    ];
-    element = $compile('<smart-checkbox model="model"></smart-checkbox>')($scope);
-    $scope.$digest();
-   
+  beforeEach(inject(function ($rootScope, _$compile_) {
+    scope = $rootScope.$new();
+    $compile = _$compile_;
+    element = null;
     
   }));
 
-
-  it('Two elements should appear', inject(function ($compile) {
-
-    debugger;  // nothing
-    dump(element);
-    dump(element.find('.smart'));
-    dump(element.find('input'));
-    dump(element.find('input.smart'));
-    expect(element.find('.smart').length).toBe(2);
-  }));
+  afterEach(function () {
+    scope.$destroy();
+  });
 
 
-/*
-  it('Element filtering by id', inject(function ($compile) {
-    scope.checkboxList = [
-      {id: '001', label:'First item'},
-      {id: '002', label:'Second item'},
-    ];
-    element = angular.element('<smart-checkbox model="checkboxList"></smart-checkbox>');
-    element = $compile(element)(scope);
-    expect(element.find('input.smart').length).toBe(2);
-    element.find('.filtercontrols input').value('001');
-    expect(element.find('input.smart').length).toBe(1);
-  }));
+  it('Two elements should appear', function () {
 
-  it('Element filtering by label', inject(function ($compile) {
-    scope.checkboxList = [
-      {id: '001', label:'First item'},
-      {id: '002', label:'Second item'},
-    ];
-    element = angular.element('<smart-checkbox model="checkboxList"></smart-checkbox>');
-    element = $compile(element)(scope);
-    scope.$digest();
-    expect(element.find('input.smart').length).toBe(2);
-    element.find('.filtercontrols input').value('Sec');
-    expect(element.find('input.smart').length).toBe(1);
-  })); */
+    element = $compile(angular.element('<smart-checkbox model="model"></smart-checkbox>'))(scope);
+    scope.$apply(function () {
+      scope.model = [
+        {id: '001', label:'First item'},
+        {id: '002', label:'Second item'},
+      ];
+    });
+    expect(element.children().eq(3).children().find('input').length).toBe(2);
+  });
+
+  it('Labels order', function () {
+
+    element = $compile(angular.element('<smart-checkbox model="model"></smart-checkbox>'))(scope);
+    scope.$apply(function () {
+      scope.model = [
+        {id: '001', label:'First item'},
+        {id: '002', label:'Second item'},
+      ];
+    });
+    expect(element.children().eq(3).find('label').eq(0).find('span').text()).toBe('[001]');
+    expect(element.children().eq(3).find('label').eq(1).find('span').text()).toBe('[002]');
+  });
+
+
+  it('Labels order (reverse)', function () {
+
+    element = $compile(angular.element('<smart-checkbox model="model"></smart-checkbox>'))(scope);
+    scope.$apply(function () {
+      scope.model = [
+        {id: '001', label:'First item'},
+        {id: '002', label:'Second item'}
+      ];
+      scope.reverse = true;
+      scope.orderby = 'id';
+    });
+    expect(element.children().eq(3).find('label').eq(0).find('span').text()).toBe('[002]');
+    expect(element.children().eq(3).find('label').eq(1).find('span').text()).toBe('[001]');
+  });
+
 });
