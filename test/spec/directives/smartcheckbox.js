47e9e915ed1e9e902ba42e537e94fd587faf1ee0
Davide Moro <davide.moro@gmail.com>
2014-01-31 01:21:42 +0100

    still error with orderby

--- test/spec/directives/smartcheckbox.js
+++ test/spec/directives/smartcheckbox.js
@@ -6,60 +6,48 @@ describe('Directive: smartCheckbox', function () {
   beforeEach(module('angularjsSmartcheckboxApp', 'views/smartcheckbox.html'));
 
   var element,
+    $rootScope,
     $compile,
     scope;
 
-  beforeEach(inject(function ($rootScope, _$compile_) {
+  beforeEach(inject(function (_$rootScope_, _$compile_) {
+    $rootScope = _$rootScope_;
     scope = $rootScope.$new();
     $compile = _$compile_;
-    element = null;
+    $rootScope.model = [
+        {id: '001', label:'First item'},
+        {id: '002', label:'Second item'}
+    ];
+
+    element = $compile(angular.element('<smart-checkbox model="model"></smart-checkbox>'))(scope);
+    $rootScope.$apply();
     
   }));
-
-  afterEach(function () {
-    scope.$destroy();
-  });
 
 
   it('Two elements should appear', function () {
 
-    element = $compile(angular.element('<smart-checkbox model="model"></smart-checkbox>'))(scope);
-    scope.$apply(function () {
-      scope.model = [
-        {id: '001', label:'First item'},
-        {id: '002', label:'Second item'},
-      ];
-    });
     expect(element.children().eq(3).children().find('input').length).toBe(2);
   });
 
-  it('Labels order', function () {
+  it('model update', function () {
 
-    element = $compile(angular.element('<smart-checkbox model="model"></smart-checkbox>'))(scope);
-    scope.$apply(function () {
-      scope.model = [
-        {id: '001', label:'First item'},
-        {id: '002', label:'Second item'},
-      ];
-    });
-    expect(element.children().eq(3).find('label').eq(0).find('span').text()).toBe('[001]');
-    expect(element.children().eq(3).find('label').eq(1).find('span').text()).toBe('[002]');
+    expect($rootScope.model.length).toBe(2);
+    expect(scope.model.length).toBe(2);
+
+    scope.model = [{id:'1', label:'1'}];
+    scope.$apply()
+
+    expect(element.children().eq(3).children().find('input').length).toBe(1);
   });
-
 
   it('Labels order (reverse)', function () {
 
-    element = $compile(angular.element('<smart-checkbox model="model"></smart-checkbox>'))(scope);
-    scope.$apply(function () {
-      scope.model = [
-        {id: '001', label:'First item'},
-        {id: '002', label:'Second item'}
-      ];
-      scope.reverse = true;
-      scope.orderby = 'id';
-    });
+    scope.reverse = true;
+    scope.orderby = 'label';
+    scope.$apply();
+
     expect(element.children().eq(3).find('label').eq(0).find('span').text()).toBe('[002]');
-    expect(element.children().eq(3).find('label').eq(1).find('span').text()).toBe('[001]');
   });
 
 });
