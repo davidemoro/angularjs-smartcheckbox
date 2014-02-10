46f71e8c1b7c66ce054b858f28bd3451c4611870
Davide Moro <davide.moro@gmail.com>
2014-02-10 21:12:44 +0100

    test filter

--- test/spec/directives/smartcheckbox.js
+++ test/spec/directives/smartcheckbox.js
@@ -18,7 +18,7 @@ describe('Directive: smartCheckbox', function () {
     $rootScope.model = [
         {id: '001', label:'First item'},
         {id: '002', label:'Second item'}
-    ];
+      ];
 
     $element = angular.element('<smart-checkbox model="model"></smart-checkbox>');
     element = $compile($element)(scope);
@@ -38,7 +38,7 @@ describe('Directive: smartCheckbox', function () {
     expect(scope.model.length).toBe(2);
 
     scope.model = [{id:'1', label:'1'}];
-    scope.$apply()
+    scope.$apply();
 
     expect($element.children().eq(3).children().find('input').length).toBe(1);
   });
@@ -50,6 +50,15 @@ describe('Directive: smartCheckbox', function () {
     $element.isolateScope().$apply();
 
     expect($element.children().eq(3).find('label').eq(0).find('span').text()).toBe('[002]');
+  });
+
+  it('Filter by label', function () {
+
+    var isolateScope = $element.isolateScope();
+    isolateScope.filter = 'Firs';
+    isolateScope.$apply();
+
+    expect($element.children().eq(3).find('label').eq(0).find('span').text()).toBe('[001]');
   });
 
 });
