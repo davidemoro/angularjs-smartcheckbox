a5be5ef43dd7904c15a628124ee2a089e0be28bd
Davide Moro <davide.moro@gmail.com>
2014-02-27 22:50:33 +0100

    presentation issues

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,9 +3,15 @@
 angular.module('angularjsSmartcheckboxApp')
   .controller('MainCtrl', ['$scope', function ($scope) {
 
-    $scope.smartList = [
+    $scope.smartlist = {};
+    $scope.smartlist.mylist = [
         {id: '001', label: 'First item'},
         {id: '002', label: 'Second item'},
         {id: 'Z01', label: 'Another item'}
       ];
+    $scope.smartlist.required = function (elem, selected) {
+        // elem is the current elem of the list
+        // selected is an array of selected elems
+        return angular.isArray(selected) ? selected.length === 0 : true;
+      };
   }]);
