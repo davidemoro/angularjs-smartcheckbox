091f0a769c44aded581b6b64d14b22df386a576b
Davide Moro <davide.moro@gmail.com>
2014-03-05 21:42:52 +0100

    tests ok, todo optimizations

--- app/scripts/directives/smartcheckbox.js
+++ app/scripts/directives/smartcheckbox.js
@@ -9,7 +9,7 @@ angular.module('angularjsSmartcheckboxApp')
       scope: {model: '=',
         requiredfn: '&?'
       },
-      controller: ['$scope', '$attrs', function ($scope, $attrs) {
+      controller: ['$scope', '$attrs', '$filter', function ($scope, $attrs, $filter) {
         // Attrs: $scope, $element, $attrs
         // TODO: Best Practice: use controller when you want to expose an API to other directives. Otherwise use link. 
         $scope.orderby = 'label';
@@ -28,6 +28,15 @@ angular.module('angularjsSmartcheckboxApp')
               // Attrs: value, key
               value.value = false;
             });
+          };
+        $scope.elements = function() {
+            var filtered, ordered;
+
+            filtered = $filter('filter')($scope.model, {$: $scope.filter});
+            ordered = $filter('orderBy')(filtered, $scope.orderby, $scope.reverse);
+
+            $scope.filtered = ordered.length ? ordered : $scope.model;
+            return $scope.filtered;
           };
         $scope.selectAll = function () {
             angular.forEach($scope.filtered, function (value) {
